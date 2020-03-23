6b3eb94e175e5ff00d12ec5c1534eac4dc89b448
Sota <sooota09@gmail.com>
2020-03-23 17:24:15 +0900

    Initial Commit Message

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,20 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+
+  def edit
+  end
+end
